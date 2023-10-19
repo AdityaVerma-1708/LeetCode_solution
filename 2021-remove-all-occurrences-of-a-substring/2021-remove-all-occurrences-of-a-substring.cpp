7b36b3cd30f8a502e40516daa6cd6d8bddf0dfe6
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2023-10-19 17:07:07 +0530

    Time: 0 ms (100%), Space: 6.9 MB (48.05%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
+
         while(s.length() != 0 && s.find(part) < s.length()){
             s.erase(s.find(part),part.length());
         }
         return s;
+        //we will traverse the array, till length of the array gets reduced to zero 
+        //or if find function finds the part and return the iterator to the first 
+        //element of the part, and if it does not find the part it will return the 
+        //end iterator which we smaller than the s.length
+
+        //inside we are erasing the part from the string
     }
 };
