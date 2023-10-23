52b9956a90ff1bd6fefc132cd5081b786094f4b5
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2023-10-23 07:06:14 +0530

    Time: 26 ms (55.7%), Space: 26.2 MB (52.22%) - LeetHub

---
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+    //approch 1:    
+        for(auto &val : nums){
+            val = val*val;
+        }
+        sort(nums.begin(),nums.end());
+        return nums;
+
+    //--optimization (without sorting)
+
+        //basically maximum element ya most negative the square hoga ya most positive
+
+        // int i=0;
+        // int j=nums.size()-1;
+        // list<int> ans;
+
+        // for(int k=nums.size()-1;k>=0;k--){
+        //     int a = nums[i]*nums[i];
+        //     int b = nums[j]*nums[j];
+
+        //     if(a>b){
+        //         ans.push_front(a);
+        //         i++;
+        //     }
+        //     else{
+        //         ans.push_front(b);
+        //         j--;
+        //     }
+        // }
+        // vector<int> anss(ans.begin(),ans.end());
+        // return anss;
+
+        //this is not fully optimize since we have used list and vector both
+
+    //optimization-2
+        
+         
+    }
+};
