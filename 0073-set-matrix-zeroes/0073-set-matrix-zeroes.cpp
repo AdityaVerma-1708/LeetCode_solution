8eca8f0722d5fd4bfb82ea021333bab12e873b99
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2023-10-27 17:39:19 +0530

    Time: 12 ms (56.09%), Space: 14 MB (10.57%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -2,28 +2,54 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
     // approch 1:
-        int m = matrix.size();
-        int n = matrix[0].size();
-        unordered_set<int> row;
-        unordered_set<int> col;
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        // int m = matrix.size();
+        // int n = matrix[0].size();
+        // unordered_set<int> row;
+        // unordered_set<int> col;
+
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(matrix[i][j] == 0){
+        //             row.insert(i);
+        //             col.insert(j);
+        //         }
+        //     }
+        // }
+
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(row.count(i) || col.count(j)){
+        //             matrix[i][j] = 0;
+        //         }
+        //     }
+        // }
+
+        
+    //approch 2: using for loop and using space
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        vector<vector<int>> temp = matrix;
+
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
                 if(matrix[i][j] == 0){
-                    row.insert(i);
-                    col.insert(j);
+                    for(int k=0;k<row;k++){
+                        temp[k][j] = 0;
+                    }
+                    for(int k=0;k<col;k++){
+                        temp[i][k] = 0;
+                    }
                 }
             }
         }
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(row.count(i) || col.count(j)){
-                    matrix[i][j] = 0;
-                }
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                matrix[i][j] = temp[i][j];
             }
         }
-    //approch 2:
 
     }
 };
