302f8e8fd6af8fe89abf46c0f99561a2d52a617b
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2023-10-29 08:26:10 +0530

    Time: 28 ms (76.57%), Space: 19.3 MB (64.1%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -48,28 +48,60 @@ public:
         // }
         // return ans;
 
-    //approch 2: stack
-        sort(intervals.begin(),intervals.end());
+    //approch 3: 
         if(intervals.size()<=1){
             return intervals;
         }
-
+        sort(intervals.begin(),intervals.end());
         vector<vector<int>> ans;
-        ans.push_back(intervals[0]);
-        int j=0;
-        for(int i=1;i<intervals.size();i++){
-            if(ans[j][1]>=intervals[i][0]){   //overlapping is there
-                int s = min(ans[j][0],intervals[i][0]);   //new starting
-                int e = max(intervals[i][1],ans[j][1]);  //new ending
-                ans.pop_back();
-                ans.push_back({s,e});
+
+        for(int i=0;i<intervals.size();i++){
+            int s = intervals[i][0];
+            int e = intervals[i][1];
+
+            if(!ans.empty() && ans.back()[1] >= e){
+                continue; //overlapping
             }
             else{
-                ans.push_back(intervals[i]);
-                j++;
+                for(int j=i+1;j<intervals.size();j++){
+                    if(intervals[j][0] <= e){ //overlapping
+                        e = max(intervals[j][1],e);
+                        s = min(intervals[j][0],s);
+                    }
+                    else{
+                        break;
+                    }
+                }
+                ans.push_back({s,e});
             }
+            
+            
         }
         return ans;
+
+
+    //approch 3: stack in single loop
+        // sort(intervals.begin(),intervals.end());
+        // if(intervals.size()<=1){
+        //     return intervals;
+        // }
+
+        // vector<vector<int>> ans;
+        // ans.push_back(intervals[0]);
+        // int j=0;
+        // for(int i=1;i<intervals.size();i++){
+        //     if(ans[j][1]>=intervals[i][0]){   //overlapping is there
+        //         int s = min(ans[j][0],intervals[i][0]);   //new starting
+        //         int e = max(intervals[i][1],ans[j][1]);  //new ending
+        //         ans.pop_back();
+        //         ans.push_back({s,e});
+        //     }
+        //     else{
+        //         ans.push_back(intervals[i]);
+        //         j++;
+        //     }
+        // }
+        // return ans;
     }
 
 
