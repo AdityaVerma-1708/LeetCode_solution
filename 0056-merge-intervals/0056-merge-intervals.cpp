4115d56876afbd2b76aa5f65db3d201c39d71464
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2023-10-29 07:59:09 +0530

    Time: 28 ms (76.57%), Space: 19.9 MB (26.23%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,76 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    //approch 1: wrong ans for some test cases
+        // vector<vector<int>> ans;
+        // for(int i=0;i<intervals.size();i++){
+        //     int s1 = intervals[i][0];
+        //     int e1 = intervals[i][1];
+
+        //     for(int j=i+1;j<intervals.size();j++){
+        //         int s2 = intervals[j][0];
+        //         int e2 = intervals[j][1];
+
+        //         if(s2>e1){
+        //             //no overlapping... push both to ans
+        //             vector<int> temp1{s1,e1};
+        //             ans.push_back(temp1);
+        //             vector<int> temp2{s2,e2};
+        //             ans.push_back(temp2);
+        //         }
+        //         else{   //overlapping
+        //             if(s1>=s2){   //second starting is dominant
+        //                 if(e2>e1){
+        //                     vector<int> temp{s2,e2};
+        //                     ans.push_back(temp);  
+        //                 }
+        //                 else{
+        //                     vector<int> temp{s2,e1};
+        //                     ans.push_back(temp);
+        //                 }
+        //                 break;
+
+        //             }
+        //             else{  //first starting is dominant
+        //                 if(e2>e1){
+        //                     vector<int> temp{s1,e2};
+        //                     ans.push_back(temp);  
+        //                 }
+        //                 else{
+        //                     vector<int> temp{s1,e1};
+        //                     ans.push_back(temp);
+        //                 }
+        //                 break;
+        //             }
+        //         }
+        //     }
+            
+        // }
+        // return ans;
+
+    //approch 2: stack
+        sort(intervals.begin(),intervals.end());
+        if(intervals.size()<=1){
+            return intervals;
+        }
+
+        vector<vector<int>> ans;
+        ans.push_back(intervals[0]);
+        int j=0;
+        for(int i=1;i<intervals.size();i++){
+            if(ans[j][1]>=intervals[i][0]){   //overlapping is there
+                int s = min(ans[j][0],intervals[i][0]);   //new starting
+                int e = max(intervals[i][1],ans[j][1]);  //new ending
+                ans.pop_back();
+                ans.push_back({s,e});
+            }
+            else{
+                ans.push_back(intervals[i]);
+                j++;
+            }
+        }
+        return ans;
+    }
+
+
+};
