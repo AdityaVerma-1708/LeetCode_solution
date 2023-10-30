7d7cfd869a350da8b1cb1814102c114d9916c15d
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2023-10-30 07:56:26 +0530

    Time: 19 ms (10.44%), Space: 10.4 MB (81%) - LeetHub

--- 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,8 +1,41 @@
 class Solution {
 public:
 
-    int numberOfOne(int n){
-        int count = 0;
+//approch 1:
+    // int numberOfOne(int n){
+    //     int count = 0;
+    //     while(n>0){
+    //         if(n%2==1){
+    //             count++;
+    //         }
+    //         n = n/2;
+    //     }
+    //     return count;
+    // }
+    // vector<int> sortByBits(vector<int>& arr) {
+    //     if(arr.size()<=1){
+    //         return arr;
+    //     }
+    //     else{   
+    //         for(int i=0;i<arr.size()-1;i++){   //bubble sort
+    //             for(int j=0;j<arr.size()-i-1;j++){
+    //                 if(numberOfOne(arr[j]) > numberOfOne(arr[j+1])){
+    //                     swap(arr[j],arr[j+1]);
+    //                 }
+    //                 else if(numberOfOne(arr[j]) == numberOfOne(arr[j+1])){
+    //                     if(arr[j]>arr[j+1]){
+    //                         swap(arr[j],arr[j+1]);
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return arr;
+    // }
+
+//aproch 2:
+    static int numberOfOne(int n){
+        int count=0;
         while(n>0){
             if(n%2==1){
                 count++;
@@ -11,23 +44,23 @@ public:
         }
         return count;
     }
-    vector<int> sortByBits(vector<int>& arr) {
+
+    static bool compareBasis(int a,int b){
+        if(numberOfOne(a) < numberOfOne(b)){
+            return true;
+        }
+        else if(numberOfOne(a) == numberOfOne(b)){
+            return a<b;
+        }
+        return false;
+    }
+
+    vector<int> sortByBits(vector<int>& arr){
         if(arr.size()<=1){
             return arr;
         }
-        else{   
-            for(int i=0;i<arr.size()-1;i++){
-                for(int j=0;j<arr.size()-i-1;j++){
-                    if(numberOfOne(arr[j]) > numberOfOne(arr[j+1])){
-                        swap(arr[j],arr[j+1]);
-                    }
-                    else if(numberOfOne(arr[j]) == numberOfOne(arr[j+1])){
-                        if(arr[j]>arr[j+1]){
-                            swap(arr[j],arr[j+1]);
-                        }
-                    }
-                }
-            }
+        else{
+            sort(arr.begin(),arr.end(),compareBasis);
         }
         return arr;
     }
