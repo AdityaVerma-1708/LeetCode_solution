85368751b0b19fee05a01bea051dc70343f98746
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2023-11-07 14:59:58 +0530

    Time: 2 ms (77.51%), Space: 6.5 MB (8.46%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -2,11 +2,27 @@ class Solution {
 public:
     int fib(int n) {
     //appoch 1: brute force (recurssion)    
+        // if(n==0 || n==1){
+        //     return n;
+        // }
+        // else{
+        //     return fib(n-1) + fib(n-2);
+        // }
+
+    //approch 2: for loop
         if(n==0 || n==1){
             return n;
         }
+        
         else{
-            return fib(n-1) + fib(n-2);
+            int prev = 0;
+            int curr = 1;
+            for(int i=2;i<=n;i++){
+                curr = curr + prev;
+                prev = curr - prev;
+            }
+            return curr;
         }
+        
     }
 };
