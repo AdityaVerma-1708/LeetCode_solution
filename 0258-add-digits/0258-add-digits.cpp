c2a1b2ada3e440f91691634e5710280d00009e8d
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2023-11-07 16:54:05 +0530

    Time: 2 ms (34.94%), Space: 6.3 MB (65.82%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -2,31 +2,31 @@ class Solution {
 public:
     int addDigits(int num) {
     //approch 1: recurrsion TLE
-        if(num<10){
-            return num;
-        }
-        else{
-            int n=0;
-            while(num > 0){
-                n = n + num%10;
-                num = num/10;
-            }
-            return addDigits(n);
-        }
-    //approch 2:
-        // if(num < 10){
+        // if(num<10){
         //     return num;
         // }
         // else{
-        //     while(num > 10){
-        //         int n = 0 ;
-        //         while(num != 0){
-        //             n = n*10 + num%10;
-        //             num = num/10;
-        //         }
-        //         num = n;
+        //     int n=0;
+        //     while(num > 0){
+        //         n = n + num%10;
+        //         num = num/10;
         //     }
-        //     return num;     
+        //     return addDigits(n);
         // }
+    //approch 2:
+        if(num < 10){
+            return num;
+        }
+        else{
+            while(num >= 10){
+                int n = 0 ;
+                while(num != 0){
+                    n = n + num%10;
+                    num = num/10;
+                }
+                num = n;
+            }
+            return num;     
+        }
     }
 };
