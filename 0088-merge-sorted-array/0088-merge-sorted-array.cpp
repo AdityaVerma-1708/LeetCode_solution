f588aea09da4d3368b58ef10b1ca90ab5afa27e5
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2023-11-08 18:32:05 +0530

    Time: 2 ms (48.25%), Space: 9.4 MB (66.81%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -2,37 +2,46 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
     //approch 1:
-        vector<int> ans;
-        int i = 0;
+        // vector<int> ans;
+        // int i = 0;
+        // int j = 0;
+        // while(i < m && j < n){
+        //     if(nums1[i] > nums2[j]){
+        //         ans.push_back(nums2[j]);
+        //         j++;
+        //     }
+        //     else if(nums1[i] < nums2[j]){
+        //         ans.push_back(nums1[i]);
+        //         i++;
+        //     }
+        //     else{
+        //         ans.push_back(nums1[i]);
+        //         ans.push_back(nums2[j]);
+        //         i++;
+        //         j++;
+        //     }
+        // }
+        // if(i != m){
+        //     while(i<m){
+        //         ans.push_back(nums1[i]);
+        //         i++;
+        //     }
+        // }
+        // if(j != n){
+        //     while(j<n){
+        //         ans.push_back(nums2[j]);
+        //         j++;
+        //     }
+        // }
+        // nums1 = ans;   
+
+    //approch 2: we should not use extra space
         int j = 0;
-        while(i < m && j < n){
-            if(nums1[i] > nums2[j]){
-                ans.push_back(nums2[j]);
-                j++;
-            }
-            else if(nums1[i] < nums2[j]){
-                ans.push_back(nums1[i]);
-                i++;
-            }
-            else{
-                ans.push_back(nums1[i]);
-                ans.push_back(nums2[j]);
-                i++;
-                j++;
-            }
+        for(int i = m;i<nums1.size();i++){
+            nums1[i] = nums2[j];
+            j++;
         }
-        if(i != m){
-            while(i<m){
-                ans.push_back(nums1[i]);
-                i++;
-            }
-        }
-        if(j != n){
-            while(j<n){
-                ans.push_back(nums2[j]);
-                j++;
-            }
-        }
-        nums1 = ans;    
+        sort(nums1.begin(),nums1.end());
+
     }
 };
