72d16d70a170319de7fbaf3c7d35cb95bb1fec00
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2023-11-11 07:58:54 +0530

    Time: 4 ms (60.26%), Space: 9.2 MB (55.83%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -2,26 +2,56 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
     //approch 1: tc ~ o(n3)
-        vector<int> ans;
-        for(int i = 0 ;i< nums1.size() ; i++){
-            int a = nums1[i];
-            for(int j = 0;j<nums2.size();j++){
-                if(a == nums2[j]){
-                    int flag = 0;
-                    for(int k = j+1;k<nums2.size();k++){
-                        if(nums2[k] > a){
-                            ans.push_back(nums2[k]);
-                            flag = 1;
-                            break;
-                        }   
-                    }
-                    if(flag == 0){
-                        ans.push_back(-1);
-                    }
-                    flag = 0;
+        // vector<int> ans;
+        // for(int i = 0 ;i< nums1.size() ; i++){
+        //     int a = nums1[i];
+        //     for(int j = 0;j<nums2.size();j++){
+        //         if(a == nums2[j]){
+        //             int flag = 0;
+        //             for(int k = j+1;k<nums2.size();k++){
+        //                 if(nums2[k] > a){
+        //                     ans.push_back(nums2[k]);
+        //                     flag = 1;
+        //                     break;
+        //                 }   
+        //             }
+        //             if(flag == 0){
+        //                 ans.push_back(-1);
+        //             }
+        //             flag = 0;
+        //         }
+        //     }
+        // }
+        // return ans;
+
+    //approch 2: using map
+       //basically we can store the next greatest element in the map
+        map<int,int> map;
+        int flag = 0;
+        for(int i=nums2.size()-1;i>=0;i--){
+            for(int j=i+1;j<nums2.size();j++){
+                if(nums2[j] > nums2[i]){
+                    map[nums2[i]] = nums2[j];
+                    flag = 1;
+                    break;
                 }
             }
+            if(flag == 0){
+                map[nums2[i]] = -1;
+            }
+            flag = 0;
         }
-        return ans;
+
+        vector<int> ans;
+        for(auto val :  nums1){
+            if(map.find(val) != map.end()){
+                auto iterator = map.find(val);
+                ans.push_back(iterator->second);
+            }
+            else{
+                ans.push_back(-1);
+            }
+        }
+        return ans;      
     }
 };
