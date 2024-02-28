d4b0fc1ae912ae45eb04eb6aeb156a6ed3b6a557
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2024-02-28 07:53:51 +0530

    Time: 7 ms (84.08%), Space: 10.3 MB (65.06%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -52,53 +52,71 @@ public:
         // int max = *(sett.rbegin());
         // return max;
 
-    //approch 2:
-        int ans = 0;
-        int count = 1;
+    //approch 2:  O(n2)
+        // int ans = 0;
+        // int count = 1;
+        // int i = 0;
+        // int j = 1;
+        // int flag = 0;
+        // if(s.size() <= 1){
+        //     return s.size();
+        // }
+        // else if(s.size()==2){     
+        //     if(s[0] == s[1]){
+        //         return 1;
+        //     }
+        //     else{
+        //         return 2;
+        //     }
+        // }  
+        // else{
+        //     while(j<s.size()){
+        //         if(s[i] == s[j]){
+        //             ans = max(count,ans);
+        //             count = 1;
+        //             i++;
+        //             j = i+1;
+        //         }
+        //         else{
+        //             for(int k = i;k<j;k++){
+        //                 if(s[k] == s[j]){
+        //                     flag = 1;
+        //                     break;
+        //                 }
+        //             }
+        //             if(flag == 0){
+        //                 count++;
+        //                 j++;
+        //                 ans = max(ans,count);
+        //             }
+        //             else{
+        //                 ans = max(ans,count);
+        //                 count = 1;
+        //                 i++;
+        //                 j = i + 1;
+        //             }
+        //         }
+        //         flag = 0; 
+        //     }
+        // } 
+        // return ans;
+
+    //appraoch 3: using hashing 
+        vector<int> seen(256,-1);
         int i = 0;
-        int j = 1;
-        int flag = 0;
-        if(s.size() <= 1){
-            return s.size();
+        int j = 0;
+        int ans = 0;
+
+        while(j<s.size()){
+            if(seen[s[j]] != -1){
+                i = max(seen[s[j]] + 1,i);
+            }
+
+            seen[s[j]] = j;
+
+            ans = max(ans,j - i + 1);
+            j++;
         }
-        else if(s.size()==2){     
-            if(s[0] == s[1]){
-                return 1;
-            }
-            else{
-                return 2;
-            }
-        }  
-        else{
-            while(j<s.size()){
-                if(s[i] == s[j]){
-                    ans = max(count,ans);
-                    count = 1;
-                    i++;
-                    j = i+1;
-                }
-                else{
-                    for(int k = i;k<j;k++){
-                        if(s[k] == s[j]){
-                            flag = 1;
-                            break;
-                        }
-                    }
-                    if(flag == 0){
-                        count++;
-                        j++;
-                        ans = max(ans,count);
-                    }
-                    else{
-                        ans = max(ans,count);
-                        count = 1;
-                        i++;
-                        j = i + 1;
-                    }
-                }
-                flag = 0; 
-            }
-        } 
         return ans;
     }
 };
