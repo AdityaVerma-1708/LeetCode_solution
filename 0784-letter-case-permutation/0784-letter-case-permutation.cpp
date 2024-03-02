3dc5415a5aac149b1193f4c46c513a98e3914cc6
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2024-03-02 23:06:18 +0530

    Time: 0 ms (100%), Space: 14.7 MB (32.44%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+    void solve(vector<string> &ans,string &temp,string s,int i){
+        if(i >= s.size()){
+            ans.push_back(temp);
+            return ;
+        }
+
+        if(isdigit(s[i])){
+            temp.push_back(s[i]);
+            solve(ans,temp,s,i+1);
+
+            temp.pop_back();
+        }
+        else{
+            //uppercase
+            temp.push_back(toupper(s[i]));
+            solve(ans,temp,s,i+1);
+            temp.pop_back();
+
+
+            //lowercase
+            temp.push_back(tolower(s[i]));
+            solve(ans,temp,s,i+1);
+            temp.pop_back();
+        }
+    }
+
+    vector<string> letterCasePermutation(string s) {
+    //using recurrsion
+        vector<string> ans;
+        string temp = "";
+        solve(ans,temp,s,0);
+        return ans;
+    }
+};
