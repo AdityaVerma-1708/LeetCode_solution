1db351647bd93a54f5e004585bf87f3665f5d07a
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2024-03-08 11:52:19 +0530

    Time: 0 ms (100%), Space: 21.6 MB (22.65%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
@@ -1,25 +1,60 @@
 class Solution {
 public:
     int maxFrequencyElements(vector<int>& nums) {
-    //using space
-        map<int,int> map;
-        for(int i = 0;i < nums.size();i++){
-            map[nums[i]] = map[nums[i]] + 1;
-        }
+    //approach 1: my brute force using space
+        // map<int,int> map;
+        // for(int i = 0;i < nums.size();i++){
+        //     map[nums[i]] = map[nums[i]] + 1;
+        // }
 
-        //find max 
-        int maxi = -1;
-        for(auto val : map){
-            maxi = max(maxi,val.second);
-        }
+        // //find max 
+        // int maxi = -1;
+        // for(auto val : map){
+        //     maxi = max(maxi,val.second);
+        // }
 
+        // int ans = 0;
+        // for(auto val : map){
+        //     if(val.second == maxi){
+        //         ans = ans + val.second;
+        //     }
+        // }
+
+        // return ans;
+
+
+    //appraoch 2: without space but more time
         int ans = 0;
-        for(auto val : map){
-            if(val.second == maxi){
-                ans = ans + val.second;
+        int maxip = 0;
+        int maxic = 0;
+        sort(nums.begin(),nums.end());
+
+        int i = 0;
+        int j = 0;
+        while(j<nums.size()){
+            if(nums[i] != nums[j]){
+                if(maxip < maxic){
+                    ans = maxic;
+                    maxip = maxic;
+                }
+                else if(maxip == maxic){
+                    ans = ans + maxic;
+                }
+                i = j;
+                maxic = 0;
+            }
+            else{
+                maxic++;
+                j++;
             }
         }
 
+        if(maxip < maxic){
+            ans = maxic;
+        }
+        else if(maxip == maxic){
+            ans = ans + maxic;
+        }
         return ans;
     }
 };
