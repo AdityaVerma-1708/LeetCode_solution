bd49162f71e593172b41123e4cc6e849cc465738
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2024-03-09 14:53:42 +0530

    Time: 15 ms (86.92%), Space: 24.8 MB (12.49%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -2,16 +2,23 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
     //approach 1: checking every elements neighbour O(n);
-        int ans = -1;
-        int i = 1;
-        while(i<nums.size()){
-            if(nums[i] != nums[i-1]){
-                return nums[i-1];
-            }
-            i = i+2;
-        }
-        if(ans == -1){
-            ans = nums.back();
+        // int ans = -1;
+        // int i = 1;
+        // while(i<nums.size()){
+        //     if(nums[i] != nums[i-1]){
+        //         return nums[i-1];
+        //     }
+        //     i = i+2;
+        // }
+        // if(ans == -1){
+        //     ans = nums.back();
+        // }
+        // return ans;
+
+    //approach 2: using xor
+        int ans = nums[0];
+        for(int i = 1;i<nums.size();i++){
+            ans = ans ^ nums[i];
         }
         return ans;
 
