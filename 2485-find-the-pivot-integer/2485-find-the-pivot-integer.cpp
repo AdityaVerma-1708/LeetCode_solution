8e0acaa770fd07675543f7bf02303b7d82ba6d93
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2024-03-13 07:54:31 +0530

    Time: 0 ms (100%), Space: 7.3 MB (41.86%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -2,14 +2,23 @@ class Solution {
 public:
     int pivotInteger(int n) {
     //brute force
+        // int total = n*(n+1)/2;
+        // int sum = 0;
+        // for(int i = 1;i<=n;i++){
+        //     sum  = sum + i;
+        //     if(sum == (total - sum + i)){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+    //maths
         int total = n*(n+1)/2;
-        int sum = 0;
-        for(int i = 1;i<=n;i++){
-            sum  = sum + i;
-            if(sum == (total - sum + i)){
-                return i;
-            }
-        }
+        double pivot = sqrt(total);
+
+        if(pivot - ceil(pivot) == 0){
+            return int(pivot);
+        }    
         return -1;
     }
 };
