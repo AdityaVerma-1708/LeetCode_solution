636e7ad631cd71e95f2579920f9649f0480658ac
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2024-03-16 10:05:26 +0530

    Time: 49 ms (26.39%), Space: 38.1 MB (51.24%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -20,17 +20,56 @@ public:
 
     //approach 2: little optimization    
     //prefix sum with space 
-        map<int,int> map;
-        int sum = 0;
+        // map<int,int> map;
+        // int sum = 0;
+        // int ans = 0;
+        // for(int i = 0;i<nums.size();i++){
+        //     sum = sum + nums[i];
+        //     map[sum] = map[sum] + 1;
+
+        //     if(sum == goal){
+        //         ans++;
+        //         auto itr = map.find(sum - goal);
+        //         if(sum - goal == sum){
+        //             if(itr != map.end()){
+        //                 ans = ans + itr->second - 1;
+        //             }
+        //         }
+        //         else{
+        //             if(itr != map.end()){
+        //                 ans = ans + itr->second;
+        //             }
+        //         }
+        //     } 
+        //     if(sum > goal){
+        //         auto itr = map.find(sum - goal);
+        //         if(sum - goal == sum){             //4-0 = 4
+        //             if(itr != map.end()){
+        //                 ans = ans + itr->second - 1;
+        //             }
+        //         }
+        //         else{
+        //             if(itr != map.end()){
+        //                 ans = ans + itr->second;
+        //             }
+        //         }    
+        //     } 
+        // }
+        // return ans;
+    //appraoch 3: optimized apporoach 2
+        unordered_map<int,int> map;
         int ans = 0;
-        for(int i = 0;i<nums.size();i++){
+        int sum = 0;
+        for(int i = 0;i< nums.size();i++){
             sum = sum + nums[i];
             map[sum] = map[sum] + 1;
 
-            if(sum == goal){
-                ans++;
+            if(sum >= goal){
+                if(sum == goal){
+                    ans++;
+                }
                 auto itr = map.find(sum - goal);
-                if(sum - goal == sum){
+                if(sum - goal == sum){  //means goal is 0
                     if(itr != map.end()){
                         ans = ans + itr->second - 1;
                     }
@@ -40,20 +79,7 @@ public:
                         ans = ans + itr->second;
                     }
                 }
-            } 
-            if(sum > goal){
-                auto itr = map.find(sum - goal);
-                if(sum - goal == sum){             //4-0 = 4
-                    if(itr != map.end()){
-                        ans = ans + itr->second - 1;
-                    }
-                }
-                else{
-                    if(itr != map.end()){
-                        ans = ans + itr->second;
-                    }
-                }    
-            } 
+            }
         }
         return ans;
     }
