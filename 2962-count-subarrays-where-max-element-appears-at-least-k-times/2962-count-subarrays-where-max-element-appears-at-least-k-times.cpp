7309c094f2937c73f12248cbefadc878ea12d59e
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2024-03-29 14:50:59 +0530

    Time: 125 ms (76.94%), Space: 120.1 MB (62.4%) - LeetHub

---
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -0,0 +1,93 @@
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int k) {
+    //brute force;  TLE for tc 622
+        // int maxi = INT_MIN;
+        // for(auto val : nums){
+        //     if(val > maxi){
+        //         maxi = val;
+        //     }
+        // }
+
+        // int count = 0;
+        // int ans = 0;
+        // for(int i = 0;i<nums.size();i++){
+        //     for(int j = i;j<nums.size();j++){
+        //         if(nums[j] == maxi){
+        //             count++;
+        //         }
+
+        //         if(count >= k){
+        //             ans++;
+        //         }
+        //     }
+        //     count = 0;
+        // }
+        // return ans;
+
+    //approach 2: little optimize tle  for tc 625 out of 633
+        // int maxi = INT_MIN;
+        // for(auto val : nums){
+        //     if(val > maxi){
+        //         maxi = val;
+        //     }
+        // }
+
+        // int count = 0;
+        // int ans = 0;
+
+        // for(int i = 0;i<nums.size();i++){
+        //     for(int  j = i;j<nums.size();j++){
+        //         if(nums[j] == maxi){
+        //             count++;
+        //         }
+
+        //         if(count == k){
+        //             ans = ans + nums.size() - j;
+        //             break;
+        //         }
+        //     }
+        //     count = 0;
+        // }
+
+        // return ans;
+
+    //approach 3: optimize 
+
+        int maxi = INT_MIN;
+        for(auto val : nums){
+            if(val > maxi){
+                maxi = val;
+            }
+        }
+
+        long long ans = 0;
+        long long count = 0;
+        int i = 0;
+        int j = 0;
+        int flag = 0;
+        while(j < nums.size()){
+            if(nums[j] == maxi && flag == 0){
+                count++;
+            } 
+
+            flag = 0;
+            if(count == k){
+                ans = ans + nums.size() - j;
+                flag = 1;
+                if(nums[i] == maxi){
+                    count--;
+                    i++;
+                }
+                else if(nums[i] != maxi){
+                    i++;
+                }
+            }
+            else{
+                j++;
+
+            }
+        }
+        return ans;
+    }
+};
