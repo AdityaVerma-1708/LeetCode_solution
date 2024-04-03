69ffd76f5e17357b40b4204ab423e6a678231da1
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2024-04-03 09:52:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+    bool solve(int i, int j, vector<vector<char>>& board, string word, int k,
+               vector<vector<bool>>& visited) {
+
+        if(i < 0 || i >= board.size() || j < 0 || j >= board[0].size()){
+            return false;
+        } 
+
+        // if (k >= word.size()) {
+        //     return true;
+        // }
+
+        if (toupper(board[i][j]) == toupper(word[k]) && visited[i][j] == false && k == word.size()-1) {
+            return true;
+        }
+
+        else if (toupper(board[i][j]) == toupper(word[k]) && visited[i][j] == false && k < word.size()-1) {
+            
+            visited[i][j] = true;
+            bool check = true;
+            // go up
+            check = solve(i-1, j, board, word,k+1,visited);
+
+            // go down
+            if(check == false){
+                check = solve(i+1, j, board, word, k+1,visited);
+            }
+            
+            // go left
+            if(check == false){
+                check = solve(i, j-1, board, word, k+1,visited);
+            }
+            
+            // go right
+            if(check == false){
+                check = solve(i, j+1, board, word, k+1,visited);
+            }
+            
+            if(check == true){
+                return true;
+            }
+
+            visited[i][j] = false;
+        }
+        return false;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+        // brute force
+
+        if(board.empty() || word.empty()) {
+            return false;
+        }
+
+        int k = 0;
+        vector<vector<bool>> visited(board.size(), vector<bool>(board[0].size(), false));
+
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[i].size(); j++) {
+                if (toupper(board[i][j]) == toupper(word[0])) {
+                    if (solve(i, j, board, word,k,visited) == true) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
