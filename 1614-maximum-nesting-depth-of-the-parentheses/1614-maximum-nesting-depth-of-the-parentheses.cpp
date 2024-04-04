86990d9e7cc1e50bc5387b8c4b481926e0d97a1d
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2024-04-04 10:43:30 +0530

    Time: 3 ms (40%), Space: 7.4 MB (78.96%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,31 +1,46 @@
 class Solution {
 public:
     int maxDepth(string s) {
-    //brute force
+    //brute force  --> O(n) and O(n)
+        // int count = 0;
+        // int ans = 0;
+        // stack<char> stack;
+        // for(int i = 0;i<s.size();i++){
+        //     if(s[i] == '(' || s[i] == ')'){
+        //         if(stack.empty()){
+        //             stack.push(s[i]);
+        //             count++;
+        //             ans = max(ans,count);
+        //         }
+        //         else{
+        //             if(s[i] == '('){
+        //                 stack.push(s[i]);
+        //                 count++;
+        //                 ans = max(ans,count);
+        //             }
+        //             else{
+        //                 stack.pop();
+        //                 count--;
+        //                 ans = max(ans,count);
+        //             }
+        //         }
+        //     }
+        // }
+        // return ans;
+
+    //appraoch 2:
         int count = 0;
         int ans = 0;
-        stack<char> stack;
         for(int i = 0;i<s.size();i++){
-            if(s[i] == '(' || s[i] == ')'){
-                if(stack.empty()){
-                    stack.push(s[i]);
-                    count++;
-                    ans = max(ans,count);
-                }
-                else{
-                    if(s[i] == '('){
-                        stack.push(s[i]);
-                        count++;
-                        ans = max(ans,count);
-                    }
-                    else{
-                        stack.pop();
-                        count--;
-                        ans = max(ans,count);
-                    }
-                }
+            if(s[i] == '('){
+                count++;
+                ans = max(ans,count);
             }
-        }
+            else if(s[i] == ')'){
+                count--;
+                ans = max(ans,count);
+            }
+        }  
         return ans;
     }
 };
