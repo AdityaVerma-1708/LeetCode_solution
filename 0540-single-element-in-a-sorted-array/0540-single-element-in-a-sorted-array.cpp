4313ff27ee4c18da268d479943df087824b9f418
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2024-04-13 20:23:45 +0530

    Time: 20 ms (44.24%), Space: 24.6 MB (85.75%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+
+    // int solve(int i,int j,vector<int>& nums){
+    //     //base case
+        
+    // }
+
     int singleNonDuplicate(vector<int>& nums) {
     //approach 1: checking every elements neighbour O(n);
         // int ans = -1;
@@ -16,11 +22,52 @@ public:
         // return ans;
 
     //approach 2: using xor
-        int ans = nums[0];
-        for(int i = 1;i<nums.size();i++){
-            ans = ans ^ nums[i];
-        }
-        return ans;
+        // int ans = nums[0];
+        // for(int i = 1;i<nums.size();i++){
+        //     ans = ans ^ nums[i];
+        // }
+        // return ans;
 
+    //approach 3: O(logn)
+        if(nums.size() == 1){
+            return nums[0];
+        }
+
+        int i = 0;
+        int j = nums.size()-1;
+        int ans = -1;
+
+        if(nums[0] != nums[1]){
+            return nums[0];
+        }
+        else if (nums[nums.size()-1] != nums[nums.size()-2]){
+            return nums[nums.size()-1];
+        }
+        else{
+            //basiacally before single element every number will on (even, odd) index
+            //but after single they eilll be on (odd, even)
+            //binary search 
+
+            while(i<=j){
+                int mid = (i+j)/2;
+                if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]){
+                    return nums[mid];
+                }
+                else if (mid % 2 != 0 && nums[mid] == nums[mid+1]){
+                    j = mid - 1;
+                }
+                else if(mid % 2 == 0 && nums[mid] == nums[mid-1]){
+                    j = mid - 1;
+                }
+                else if(mid % 2 != 0 && nums[mid] == nums[mid-1]){
+                    i = mid + 1;
+                }
+                else{
+                    i = mid + 1;
+                }
+
+            }
+        }
+        return 1;
     }
 };
