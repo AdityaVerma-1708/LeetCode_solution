a4436684dc34389d6f1d097b50e6053881d78211
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2024-05-30 21:35:48 +0530

    Time: 0 ms (100%), Space: 9.1 MB (55.34%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,22 +1,42 @@
 class Solution {
 public:
 
-    int find(int n){
-        int ans = 0;
-        while(n!=0){
-            if(n & 1 != 0){
-                ans++;
-            }
-            n = n  >> 1;
-        }
-        return ans;
-    }
+//appraoch 1:
+    // int find(int n){
+    //     int ans = 0;
+    //     while(n!=0){
+    //         if(n & 1 != 0){
+    //             ans++;
+    //         }
+    //         n = n  >> 1;
+    //     }
+    //     return ans;
+    // }
 
-    vector<int> countBits(int n) {
-        vector<int> ans ;
-        for(int i = 0;i<=n;i++){
-            ans.push_back(find(i));
+    // vector<int> countBits(int n) {
+    //     vector<int> ans ;
+    //     for(int i = 0;i<=n;i++){
+    //         ans.push_back(find(i));
+    //     }
+    //     return ans;
+    // }
+
+//appraoch 2:
+    vector<int> countBits(int n){
+        //bascially we can use the previous ans to find the bigger one
+        vector<int> ans(n+1);
+        ans[0] = 0;
+        for(int i = 1;i<=n;i++){
+            if(i%2 == 0){
+                //even;
+                ans[i] = ans[i>>1] ;
+            }
+            else{
+                //odd
+                ans[i] = ans[i>>1] + 1;
+            }
         }
+
         return ans;
-    }
+    }    
 };
