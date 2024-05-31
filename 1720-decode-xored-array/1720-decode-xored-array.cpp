db5d783c100f7d60701dc53f722e604031eeeea9
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2024-05-31 21:33:26 +0530

    Time: 19 ms (67.62%), Space: 28.4 MB (47.93%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.cpp
+++ 1720-decode-xored-array/1720-decode-xored-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> decode(vector<int>& encoded, int first) {
+    //approach 1: using vector   
         vector<int> ans;
         ans.push_back(first);
         for(int  i = 0;i < encoded.size();i++){
@@ -9,5 +10,6 @@ public:
 
         return ans;
 
+    
     }
 };
