570264c0c9874369884a318fcc9a966f493a15e0
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2024-07-17 19:52:18 +0530

    Time: 118 ms (52%), Space: 126.8 MB (5.8%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,5 +1,98 @@
 class Solution {
 public:
+//approach 1: BS
+
+    // int maxi(vector<int> arr){
+    //     int ans = INT_MIN;
+    //     for(int i = 0;i<arr.size();i++){
+    //         ans = max(ans,arr[i]);
+    //     }
+    //     return ans;
+    // }
+    // int mini(vector<int> arr){
+    //     int ans = INT_MAX;
+    //     for(int i = 0;i<arr.size();i++){
+    //         ans = min(ans,arr[i]);
+    //     }
+    //     return ans;
+    // }
+
+    // int conseflower(vector<int> arr,int day,int k){
+    //     if(k == 1){
+    //         int ans = 0;
+    //         for(int i = 0;i<arr.size();i++){
+    //             if(arr[i] <= day){
+    //             ans++;
+    //             }
+    //         }
+    //         return ans;
+    //     }
+    //     else{
+    //         int i = 0;
+    //         int j = 0;
+    //         int ans = 0;
+
+    //         int temp = 0;
+    //         while(j < arr.size()){
+    //             if(j-i + 1 != k){
+    //                 if(arr[j] <= day){
+    //                     temp++;
+    //                 }
+    //                 j++;
+    //             }
+    //             else{
+    //                 if(arr[j] <= day){
+    //                     temp++; 
+    //                 }
+    //                 if(temp == k){
+    //                     ans++;
+    //                     j++;
+    //                     i = j;
+    //                     temp = 0;
+    //                 }
+    //                 else{
+    //                     if(arr[i] <= day){
+    //                         temp--;
+    //                     }
+    //                     i++;
+    //                     j++;
+    //                 }
+    //             }
+    //         }
+    //         cout<<ans<<endl;
+    //         return ans;
+    //     }
+    // }
+
+    // int minDays(vector<int>& bloomDay, int m, int k) {
+    // //appraoch 1L:
+    //     long long total = (long long)m * k; 
+    //     if(bloomDay.size() < total){
+    //         return -1;
+    //     }
+    //     else{
+    //         int i = mini(bloomDay); 
+    //         int j = maxi(bloomDay);
+    //         int ans = INT_MAX;
+
+    //         while(i<=j){
+    //             int mid = (i+j)/2;
+
+    //             if(conseflower(bloomDay,mid,k) >= m){
+    //                 ans = min(ans,mid);
+    //                 j = mid - 1;
+    //             }
+    //             else{
+    //                 i = mid + 1;
+    //             }
+    //         }
+    //         return ans;
+    //     }
+    // }
+
+
+//approch 2: BS wiht little clean code
+
     int maxi(vector<int> arr){
         int ans = INT_MIN;
         for(int i = 0;i<arr.size();i++){
@@ -7,52 +100,28 @@ public:
         }
         return ans;
     }
+    int mini(vector<int> arr){
+        int ans = INT_MAX;
+        for(int i = 0;i<arr.size();i++){
+            ans = min(ans,arr[i]);
+        }
+        return ans;
+    }
 
     int conseflower(vector<int> arr,int day,int k){
-        if(k == 1){
-            int ans = 0;
-            for(int i = 0;i<arr.size();i++){
-                if(arr[i] <= day){
-                ans++;
-                }
+        int count = 0;
+        int ans = 0;
+        for(int i = 0;i<arr.size();i++){
+            if(arr[i] <= day){
+                count++;
             }
-            return ans;
-        }
-        else{
-            int i = 0;
-            int j = 0;
-            int ans = 0;
-
-            int temp = 0;
-            while(j < arr.size()){
-                if(j-i + 1 != k){
-                    if(arr[j] <= day){
-                        temp++;
-                    }
-                    j++;
-                }
-                else{
-                    if(arr[j] <= day){
-                        temp++; 
-                    }
-                    if(temp == k){
-                        ans++;
-                        j++;
-                        i = j;
-                        temp = 0;
-                    }
-                    else{
-                        if(arr[i] <= day){
-                            temp--;
-                        }
-                        i++;
-                        j++;
-                    }
-                }
+            else{
+                ans = ans + (count/k);
+                count = 0;
             }
-            cout<<ans<<endl;
-            return ans;
         }
+        ans = ans + (count/k);
+        return ans;
     }
 
     int minDays(vector<int>& bloomDay, int m, int k) {
@@ -62,7 +131,7 @@ public:
             return -1;
         }
         else{
-            int i = 1; //minium wait
+            int i = mini(bloomDay); 
             int j = maxi(bloomDay);
             int ans = INT_MAX;
 
@@ -80,4 +149,5 @@ public:
             return ans;
         }
     }
+
 };
