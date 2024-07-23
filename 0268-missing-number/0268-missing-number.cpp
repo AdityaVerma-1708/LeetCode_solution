cef2ab8126a179ef0a5338599037a202223ea339
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2024-07-23 23:48:10 +0530

    Time: 4 ms (98.74%), Space: 20.7 MB (28.23%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -12,16 +12,28 @@ public:
         // }
         // return i;
     //approach 2: use space and O(n)
-        vector<int> arr(nums.size()+1,0);
-        for(int i = 0;i<nums.size();i++){
-            arr[nums[i]] = 1;
+        // vector<int> arr(nums.size()+1,0);
+        // for(int i = 0;i<nums.size();i++){
+        //     arr[nums[i]] = 1;
+        // }
+
+        // for(int i = 0;i<arr.size();i++){
+        //     if(arr[i] == 0){
+        //         return i;
+        //     }
+        // }
+        // return 1;
+
+    //approach 3:
+        int n = nums.size();
+        int sum1 = n*(n+1)/2;
+        int sum2 = 0;
+        for(int i = 0;i<n;i++){
+            sum2 = sum2 + nums[i];
         }
 
-        for(int i = 0;i<arr.size();i++){
-            if(arr[i] == 0){
-                return i;
-            }
-        }
-        return 1;
+        return sum1 - sum2;
+
+    //appraoch 4: using xor 
     }
 };
