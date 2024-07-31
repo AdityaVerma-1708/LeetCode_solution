5c9e8ec1d4b24d5ef61c7e69cb149b2bc1a6046a
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2024-07-31 11:13:08 +0530

    Time: 0 ms (100%), Space: 12.2 MB (36.49%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -2,27 +2,61 @@ class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
     //appraoch 1:
-        int ans = -1;
-        int count = 0;
-        int num = 1;
+        // int ans = -1;
+        // int count = 0;
+        // int num = 1;
+        // int i = 0;
+        // while(i<arr.size()){
+        //     if(arr[i] == num){
+        //         i++;
+        //         num++;
+        //     }
+        //     else{
+        //         count++;
+        //         if(count == k){
+        //             ans = num;
+        //             break;
+        //         }
+        //         num++;
+        //     }
+        // }
+        // if(ans == -1 ){
+        //     return arr.back() + k - count;
+        // }
+
+        // return ans;
+
+    //approach 2: 
+
+    //basically first i started from back and exploring 
+    /*in the position of 11 it should be 5 if nonn of missing so there will be 11 - 5 missing 
+    number behind 11 then we are checking it with k and moving accordingly
+    
+    at the end we need range of two indices where the missing number should be present*/
+
+        if(k < arr[0]){
+            return k;
+        }
+        if(k > arr[arr.size()-1] - arr.size()){
+            return k + arr.size();
+        }
+
         int i = 0;
-        while(i<arr.size()){
-            if(arr[i] == num){
-                i++;
-                num++;
+        int j = arr.size()-1;
+        int leftnear = i;
+        int rightnear = j;
+
+        while(i<=j){
+            int mid = (i+j)/2;
+            if(arr[mid] - (mid+1) < k){
+                leftnear = mid;
+                i = mid + 1;
             }
             else{
-                count++;
-                if(count == k){
-                    ans = num;
-                    break;
-                }
-                num++;
+                rightnear = mid;
+                j = mid - 1;
             }
         }
-        if(ans == -1){
-            return arr.back() + k;
-        }
-        return ans;
+        return arr[leftnear] + (k - (arr[leftnear] - (leftnear + 1))) ;
     }
 };
