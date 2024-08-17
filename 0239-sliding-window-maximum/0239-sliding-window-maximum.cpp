a5c43d7a0cc6a09efae155e05fbd8a2fa5b83e51
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2024-08-17 18:01:51 +0530

    Time: 177 ms (47.12%), Space: 137.4 MB (34.6%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-    //appraoch 1: TLE at 37
+    //appraoch 1: TLE at 37 /51 
         // vector<int> ans;
         // int i = 0;
         // int j = k - 1;
@@ -19,7 +19,7 @@ public:
 
         // return ans;
 
-    //approach 2: TLE at 37 
+    //approach 2: TLE at 37 /51
 
         // vector<int> ans;
         // deque<int> dq   ;
@@ -48,53 +48,79 @@ public:
         // }
         // return ans;  
     
-    //approachh 3:
-        vector<int> ans;
-        deque<int> dq ;
-        int i = 0;
-        int j = k - 1;
+    //approachh 3: TLE @ 47/51
+        // vector<int> ans;
+        // deque<int> dq ;
+        // int i = 0;
+        // int j = k - 1;
         
-        int maxi = INT_MIN;
-        int index = 0;
-        for(int m = i;m<=j;m++){
-            dq.push_back(nums[m]);
-            if(maxi < nums[m]){
-                maxi = nums[m];
-                index = m;
+        // int maxi = INT_MIN;
+        // int index = 0;
+        // for(int m = i;m<=j;m++){
+        //     dq.push_back(nums[m]);
+        //     if(maxi < nums[m]){
+        //         maxi = nums[m];
+        //         index = m;
+        //     }
+        // }
+
+        // while(j<nums.size()){
+        //     ans.push_back(maxi);
+
+        //     if(nums[i] == maxi){
+        //         dq.pop_front();
+        //         i++;
+        //         j++;
+        //         if(j < nums.size()){
+        //             dq.push_back(nums[j]);
+        //             maxi = INT_MIN;
+        //             for(int m = i;m<=j;m++){
+        //                 if(maxi < nums[m]){
+        //                     maxi = nums[m];
+        //                     index = m;
+        //                 }
+        //             }
+        //         }
+        //     }
+        //     else{
+        //         dq.pop_front();
+        //         i++;
+        //         j++;
+        //         if(j < nums.size() && nums[j] >= maxi){
+        //             maxi = nums[j];
+        //             index = j;
+        //         }
+        //         if(j< nums.size()){
+        //             dq.push_back(nums[j]);
+        //         }
+        //     }
+        // }
+        // return ans;
+
+    //appraoch 4: //basically we should not search maxi again and again
+        //monotonic dqueue
+        vector<int> ans;
+        deque<int> dq;//storing the indices
+        for(int i = 0;i<nums.size();i++){
+            while(!dq.empty() && dq.front() == i-k){
+                dq.pop_front();
+            }
+
+            while(!dq.empty() && nums[dq.back()] < nums[i]){
+                dq.pop_back();
+            }
+
+            dq.push_back(i);
+
+            if(i >= (k-1)){
+                ans.push_back(nums[dq.front()]);
             }
         }
 
-        while(j<nums.size()){
-            ans.push_back(maxi);
-
-            if(nums[i] == maxi){
-                dq.pop_front();
-                i++;
-                j++;
-                if(j < nums.size()){
-                    dq.push_back(nums[j]);
-                    maxi = INT_MIN;
-                    for(int m = i;m<=j;m++){
-                        if(maxi < nums[m]){
-                            maxi = nums[m];
-                            index = m;
-                        }
-                    }
-                }
-            }
-            else{
-                dq.pop_front();
-                i++;
-                j++;
-                if(j < nums.size() && nums[j] >= maxi){
-                    maxi = nums[j];
-                    index = j;
-                }
-                if(j< nums.size()){
-                    dq.push_back(nums[j]);
-                }
-            }
-        }
         return ans;
+
+        
+        
+
     }
 };
