adbed3151ba859decbcba3a07c038503b199d349
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2024-08-18 14:29:54 +0530

    Time: 58 ms (26.87%), Space: 16.9 MB (8.55%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -4,46 +4,34 @@ public:
     //brute force:
         int i = 0;
         int j = 0;
-        int ans1 = 0; 
+        int ans = 0; 
         unordered_map<char,int> map;
+
         while(j < s.size()){
             map[s[j]]++;
+
             if(map.size() == 3){
-                ans1 = ans1 + s.size() - j;
+                ans = ans + s.size() - j;
+
                 while(map.size() == 3){
                     map[s[i]]--;
+
                     if(map[s[i]] == 0){
                         map.erase(s[i]);
                     }
+
                     i++;
+
                     if(map.size() == 3){
-                        ans1 = ans1 + s.size() - j;
+                        ans = ans + s.size() - j;
                     }
+
                 }
+
             }
             j++;
         }   
 
-        // j = s.size()-1;
-        // i = s.size()-1;
-        // int ans2 = 0;
-        // map.clear();
-        // while(j > 0){
-        //     map[s[j]]++;
-        //     if(map.size() == 3){
-        //         ans2 = ans2 + j + 1;
-        //         while(map.size() == 3){
-        //             map[s[i]]--;
-        //             if(map[s[i]] == 0){
-        //                 map.erase(s[i]);
-        //             }
-        //             i--;
-        //         }
-        //     }
-        //     j--;
-        // }
-
-        // return max(ans1,ans2);
-        return ans1;
+        return ans;
     }
 };
