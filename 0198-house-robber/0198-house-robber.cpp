f5c6688c83a2bdb489441cffe5652acb7b14c21b
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2024-08-25 11:47:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -33,23 +33,70 @@ public:
 
 
 
-//appraoch 2: top bottom dp (memoization)
+//appraoch 2: top bottom dp (memoization) from 0 to end
 
-    int solve(vector<int>& nums,int i,vector<int>& dp){
-        if(i >= nums.size()){
-            return 0;
-        }
+    // int solve(vector<int>& nums,int i,vector<int>& dp){
+    //     if(i >= nums.size()){
+    //         return 0;
+    //     }
 
-        if(dp[i] != -1){
-            return dp[i];
-        }
+    //     if(dp[i] != -1){
+    //         return dp[i];
+    //     }
 
-        return dp[i] = max(nums[i] + solve(nums,i+2,dp) , solve(nums,i+1,dp));
+    //     return dp[i] = max(nums[i] + solve(nums,i+2,dp) , solve(nums,i+1,dp));
 
-    }
+    // }
+
+    // int rob(vector<int>& nums) {
+    //     vector<int> dp(nums.size(),-1);
+    //     return solve(nums,0,dp);
+    // }
+
+//approach 3: memoizaton from nums.size() to 0
+
+    // int solve(vector<int>& nums,int i,vector<int>& dp){
+    //     if(i < 0){
+    //         return 0;
+    //     }
+
+    //     if(i == 0){
+    //         return nums[i];
+    //     }
+        
+    //     if(dp[i] != -1){
+    //         return dp[i];
+    //     }
+        
+
+    //     return dp[i] = max(nums[i] + solve(nums,i-2,dp) , solve(nums,i-1,dp));
+
+    // }
+
+    // int rob(vector<int>& nums) {
+    //     vector<int> dp(nums.size(),-1);
+    //     return solve(nums,nums.size()-1,dp);
+    // }
+
+//appraoch 4: bottom up DP (tabular)    
 
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size(),-1);
-        return solve(nums,0,dp);
+        vector<int> dp(nums.size()+1,-1);
+        dp[0] = nums[0];
+        dp[1] = nums[1];
+
+        for(int i = 2;i<nums.size();i++){
+            int maxi = 0;
+            for(int j = i-2;j >= 0;j--){
+                maxi = max(nums[i] + dp[j],maxi);
+            }
+            dp[i] = maxi;
+        }
+
+        int ans = 0;
+        for(int i = 0;i<dp.size();i++){
+            ans = max(ans,dp[i]);
+        }
+        return ans;
     }
 };
