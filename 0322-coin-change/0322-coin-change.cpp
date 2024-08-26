fa43c83db1ed911c2164fa968ca7749d73c8da2d
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2024-08-26 22:28:47 +0530

    Time: 62 ms (13.11%), Space: 22.5 MB (48.77%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,26 +1,60 @@
 class Solution {
 public:
 
-    int solve(vector<int>& coins,int amount,long long curr,long long ans){
-        if(curr > amount){
+//approach 1: using recurrsion TLE 
+    // int solve(vector<int>& coins,int amount,long long curr,long long ans){
+    //     if(curr > amount){
+    //         return INT_MAX;
+    //     }
+    //     if(curr == amount){
+    //         return ans;
+    //     }
+
+    //     int count = INT_MAX;
+    //     for(int i = 0;i<coins.size();i++){
+    //         count = min(solve(coins,amount,curr+coins[i],ans+1),count);
+    //     }
+    //     return count;
+
+    // }
+
+    // int coinChange(vector<int>& coins, int amount) {
+    //     if(solve(coins,amount,0,0) == INT_MAX){
+    //         return -1;
+    //     }
+    //     return solve(coins,amount,0,0);
+    // }
+
+//approach 2: dp with memoization top down 
+    
+    int solve(vector<int>& coins,int amount,vector<int>& dp){
+        if(amount < 0){
             return INT_MAX;
         }
-        if(curr == amount){
-            return ans;
+        if(amount == 0){
+            return 0;
+        }
+
+        if(dp[amount] != -1){
+            return dp[amount];
         }
 
         int count = INT_MAX;
         for(int i = 0;i<coins.size();i++){
-            count = min(solve(coins,amount,curr+coins[i],ans+1),count);
+            if(solve(coins,amount-coins[i],dp) == INT_MAX){
+                continue;
+            }
+            count = min(count, 1 + solve(coins,amount-coins[i],dp));
         }
-        return count;
 
+        return dp[amount] = count;
     }
 
     int coinChange(vector<int>& coins, int amount) {
-        if(solve(coins,amount,0,0) == INT_MAX){
+        vector<int> dp(10001,-1);
+        if(solve(coins,amount,dp) == INT_MAX){
             return -1;
         }
-        return solve(coins,amount,0,0);
+        return solve(coins,amount,dp);
     }
 };
