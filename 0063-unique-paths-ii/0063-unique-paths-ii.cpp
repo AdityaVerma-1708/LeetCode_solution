f5dc03ac4423455d0b7edb99fe0720b26149e7aa
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2024-09-10 23:41:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,9 +1,45 @@
 class Solution {
 public:
 
-//approach 1: recurrsion
+//approach 1: recurrsion TLE
 
-    int solve(vector<vector<int>>& mat,int i,int j,int m,int n,vector<vector<int>>& dp){
+    // int solve(vector<vector<int>>& mat,int i,int j,int m,int n){
+    //     if(i == m-1 && j == n-1){
+    //         return 1;
+    //     }
+
+    //     if(mat[i][j] == 1){
+    //         return 0;
+    //     }
+
+    //     int count = 0;
+    //     //go right
+    //     if(j+1 < n){
+    //         count = count + solve(mat,i,j+1,m,n);
+    //     }
+
+    //     //go down
+    //     if(i+1 < m){
+    //         count = count + solve(mat,i+1,j,m,n);
+    //     }
+
+    //     return count;
+    // }
+
+    // int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+    //     int m = obstacleGrid.size();
+    //     int n = obstacleGrid[0].size();
+        
+    //     if(obstacleGrid[m-1][n-1] == 1){
+    //         return 0;
+    //     }
+
+    //     return solve(obstacleGrid,0,0,m,n);
+    // }
+
+//appraoch 2: dp with memoization bottom up
+
+    int solve(vector<vector<int>>& mat,int i,int j,int m,int n,vector<vector<int>>&dp){
         if(i == m-1 && j == n-1){
             return 1;
         }
@@ -19,12 +55,12 @@ public:
         int count = 0;
         //go right
         if(j+1 < n){
-            count = count + solve(mat,i,j+1,m,n,dp);
+            count = count + solve(mat,i,j+1,m,n);
         }
 
         //go down
         if(i+1 < m){
-            count = count + solve(mat,i+1,j,m,n,dp);
+            count = count + solve(mat,i+1,j,m,n);
         }
 
         return dp[i][j] = count;
@@ -33,13 +69,12 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-
+        
         if(obstacleGrid[m-1][n-1] == 1){
             return 0;
         }
 
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        
         return solve(obstacleGrid,0,0,m,n,dp);
     }
 };
