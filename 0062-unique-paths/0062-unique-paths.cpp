8687920596400a4eb101d5fe9f3e9164c8079485
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2024-09-18 15:24:10 +0530

    Time: 0 ms (100%), Space: 8 MB (21.86%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -25,7 +25,7 @@ public:
     //     return solve(0,0,m,n);
     // }
 
-//appraoch 2: Dp memoization starting from start to end
+//appraoch 2: Dp memoization start to end (bottom up)
 
     // int solve(int i,int j,int m,int n,vector<vector<int>>&dp){
     //     if(i == m-1 && j == n-1){
@@ -56,33 +56,66 @@ public:
     // }
 
 
-//appraoch 2: Dp memoization end to start
+//appraoch 3: Dp memoization end to start (top down)
 
-    int solve(int m,int n,vector<vector<int>>&dp){
-        if(m == 0 && n == 0){
-            return 1;
-        }
+    // int solve(int m,int n,vector<vector<int>>&dp){
+    //     if(m == 0 && n == 0){
+    //         return 1;
+    //     }
 
-        if(dp[m][n] != -1){
-            return dp[m][n];
-        }
+    //     if(dp[m][n] != -1){
+    //         return dp[m][n];
+    //     }
 
-        int count = 0;
-        //right if possible
-        if(n-1 >= 0){
-            count = count + solve(m,n-1,dp);
-        }
+    //     int count = 0;
+    //     //right if possible
+    //     if(n-1 >= 0){
+    //         count = count + solve(m,n-1,dp);
+    //     }
 
-        //down if possible
-        if(m-1 >= 0){
-            count = count + solve(m-1,n,dp);
-        }
+    //     //down if possible
+    //     if(m-1 >= 0){
+    //         count = count + solve(m-1,n,dp);
+    //     }
 
-        return dp[m][n] = count;
-    }
+    //     return dp[m][n] = count;
+    // }
+
+    // int uniquePaths(int m, int n) {
+    //     vector<vector<int>> dp(m,vector<int>(n,-1));
+    //     return solve(m-1,n-1,dp);
+    // }
+
+
+//appraoch 4: tabular dp bottom up
 
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        return solve(m-1,n-1,dp);
+        
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(i == 0 && j == 0){
+                    dp[i][j] = 1;
+                }
+                else{
+                    int count = 0;
+                    //up if possible
+                    if(i > 0){
+                        count = count + dp[i-1][j];
+                    }
+
+                    //left
+                    if(j > 0){
+                        count = count + dp[i][j-1];
+                    }
+
+                    dp[i][j] = count;
+                }
+
+                
+            }
+        }
+
+        return dp[m-1][n-1];
     }
 };
