fa7b5186608ee4755c723c9a557b5ca4b82bf0c8
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2024-09-19 09:26:09 +0530

    Time: 21 ms (5.11%), Space: 15.1 MB (5.43%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,9 +1,52 @@
 class Solution {
 public: 
 
-//approach 1: recurrsion 
+//approach 1: recurrsion TLE@ 38/52
+    // int solve(vector<vector<int>>& mat,int i,int j,int m,int n){
+    //     if(i == m){
+    //         return 0;
+    //     }
 
-    int solve(vector<vector<int>>& mat,int i,int j,int m,int n){
+    //     int minus = INT_MAX;
+    //     int same = INT_MAX; 
+    //     int plus = INT_MAX;
+
+    //     if(j-1 >= 0){
+    //         minus = min(minus,mat[i][j-1] + solve(mat,i+1,j-1,m,n));
+    //     } 
+
+    //     same = min(same,mat[i][j] + solve(mat,i+1,j,m,n));
+
+    //     if(j+1 < n){
+    //         plus = min(plus,mat[i][j+1] + solve(mat,i+1,j+1,m,n));
+    //     }
+
+    //     return min({minus,same,plus});
+    // }
+
+    // int minFallingPathSum(vector<vector<int>>& matrix) {
+    //     int m = matrix.size();
+    //     int n = matrix[0].size();
+
+    //     int ans = INT_MAX;
+
+    //     if(m == 1){
+    //         for(int j = 0;j < n;j++){
+    //             ans = min(ans,matrix[0][j]);
+    //         }
+    //         return ans;
+    //     }
+
+    //     for(int j = 0; j < n;j++){
+    //         ans = min(ans,matrix[0][j] + solve(matrix,1,j,m,n));
+    //     }
+        
+    //     return ans;   
+    // }
+
+//approach 2:
+
+    int solve(vector<vector<int>>& mat,int i,int j,int m,int n,vector<vector<int>>&dp){
         if(i == m){
             return 0;
         }
@@ -12,17 +55,21 @@ public:
         int same = INT_MAX; 
         int plus = INT_MAX;
 
-        if(j-1 >= 0){
-            minus = min(minus,mat[i][j-1] + solve(mat,i+1,j-1,m,n));
-        } 
-
-        same = min(same,mat[i][j] + solve(mat,i+1,j,m,n));
-
-        if(j+1 < n){
-            plus = min(plus,mat[i][j+1] + solve(mat,i+1,j+1,m,n));
+        if(dp[i][j] != INT_MAX){
+            return dp[i][j];
         }
 
-        return min({minus,same,plus});
+        if(j-1 >= 0){
+            minus = min(minus,mat[i][j-1] + solve(mat,i+1,j-1,m,n,dp));
+        } 
+
+        same = min(same,mat[i][j] + solve(mat,i+1,j,m,n,dp));
+
+        if(j+1 < n){
+            plus = min(plus,mat[i][j+1] + solve(mat,i+1,j+1,m,n,dp));
+        }
+
+        return dp[i][j] = min({minus,same,plus});
     }
 
     int minFallingPathSum(vector<vector<int>>& matrix) {
@@ -37,12 +84,13 @@ public:
             }
             return ans;
         }
+        vector<vector<int>>dp(m,vector<int>(n,INT_MAX));
 
         for(int j = 0; j < n;j++){
-            ans = min(ans,matrix[0][j] + solve(matrix,1,j,m,n));
+            dp[0][j] = min(dp[0][j],matrix[0][j] + solve(matrix,1,j,m,n,dp));
+            ans = min(ans,dp[0][j]);
         }
         
-        return ans;
-        
+        return ans;   
     }
 };
