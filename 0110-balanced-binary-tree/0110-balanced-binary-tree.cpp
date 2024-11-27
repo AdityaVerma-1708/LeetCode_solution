c0b083c6746fe74dc3bbe052769f722f94e67fcf
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2024-11-27 12:54:32 +0530

    Time: 0 ms (100%), Space: 23.2 MB (8.51%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -12,57 +12,98 @@
 class Solution {
 public:
 //approach 1: brute force
+    // int findsize(TreeNode* node,int count){
+    //     if(node->left == nullptr && node->right == nullptr){
+    //         return count;
+    //     }
 
-    int findsize(TreeNode* node,int count){
+    //     int left = 0;
+    //     int right = 0;
+
+    //     if(node->left != nullptr){
+    //         left = findsize(node->left,count+1);
+    //     }
+
+    //     if(node->right != nullptr){
+    //         right = findsize(node->right,count+1);
+    //     }
+
+    //     return max(left,right);
+    // }
+
+
+    // bool isBalanced(TreeNode* root) {
+    //     if(root == nullptr){
+    //         return true;
+    //     }
+    //     else if (root->left == nullptr && root->right == nullptr){
+    //         return true;
+    //     }
+    //     else{
+    //         int left = 0;
+    //         int right = 0;
+
+    //         if(root->left !=  nullptr){
+    //             left = findsize(root->left,1);
+    //         }
+
+    //         if(root->right != nullptr){
+    //             right = findsize(root->right,1);
+    //         }
+
+    //         if(abs(left-right) > 1){
+    //             return false;
+    //         }
+    //         else{
+    //             if(isBalanced(root->left) && isBalanced(root->right)){
+    //                 return true;
+    //             }
+    //             else{
+    //                 return false;
+    //             }
+    //         }
+    //     }
+    // }
+
+//approach 2:
+
+    int solve(TreeNode* node, int count){
         if(node->left == nullptr && node->right == nullptr){
             return count;
         }
 
-        int left = 0;
-        int right = 0;
+        int left = count;
+        int right = count;
 
         if(node->left != nullptr){
-            left = findsize(node->left,count+1);
+            left = solve(node->left,count+1);
         }
 
         if(node->right != nullptr){
-            right = findsize(node->right,count+1);
+            right = solve(node->right,count+1);
+        }
+
+        if(left == -1 || right == -1){
+            return -1;
+        }
+
+        if(abs((left-count) - (right-count)) > 1){
+            return -1;
         }
 
         return max(left,right);
     }
 
-
     bool isBalanced(TreeNode* root) {
         if(root == nullptr){
             return true;
         }
-        else if (root->left == nullptr && root->right == nullptr){
-            return true;
+
+        if(solve(root,1) == -1){
+            return false;
         }
         else{
-            int left = 0;
-            int right = 0;
-
-            if(root->left !=  nullptr){
-                left = findsize(root->left,1);
-            }
-
-            if(root->right != nullptr){
-                right = findsize(root->right,1);
-            }
-
-            if(abs(left-right) > 1){
-                return false;
-            }
-            else{
-                if(isBalanced(root->left) && isBalanced(root->right)){
-                    return true;
-                }
-                else{
-                    return false;
-                }
-            }
+            return true;
         }
     }
 };
