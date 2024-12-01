887cf5cbd7982409d36345f0fe7740273bead9de
ADITYA VERMA <130679524+AdityaVerma-1708@users.noreply.github.com>
2024-12-01 11:45:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -2,17 +2,39 @@ class Solution {
 public:
 //APPROACH 1: burte force
 
+    // bool checkIfExist(vector<int>& arr) {
+    //     for(int i = 0;i<arr.size();i++){
+    //         for(int j = 0;j<arr.size();j++){
+    //             if(arr[i] == 2*arr[j] && i != j){
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
+
+//approach 2: little optimised
+
     bool checkIfExist(vector<int>& arr) {
+        // unordered_map<int,int> map;  //we can use set also same time complexity
+        set<int> set;
         for(int i = 0;i<arr.size();i++){
-            for(int j = 0;j<arr.size();j++){
-                if(arr[i] == 2*arr[j] && i != j){
-                    return true;
+            if(set.empty()){
+                set.insert(arr[i]);
+            }
+            else{
+                if(arr[i]%2 == 0){
+                    if(set.find(arr[i]/2) != set.end()){
+                        return true;
+                    }
+                }
+                else{
+                    if(set.find(arr[i]*2) != set.end()){
+                        return true;
+                    }
                 }
             }
         }
         return false;
     }
-    // bool checkIfExist(vector<int>& arr) {
-
-    // }
 };
